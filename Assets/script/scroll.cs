83423db71e1f9422f6126e382816a1baf4dd6396
Guidobeck <148489504+Reidemon@users.noreply.github.com>
2023-10-30 09:36:30 -0300

    Update scroll.cs

--- Assets/script/scroll.cs
+++ Assets/script/scroll.cs
@@ -24,7 +24,7 @@ public class scroll : MonoBehaviour
     {
         if(transform.position.y < -spriteWidth)
         {
-            SceneManager.LoadSceneAsync(1);
+            SceneManager.LoadSceneAsync(0);
             ResetPosition();
         }
     }
